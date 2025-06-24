fa3115acad39153b65365d6cd4e1a31e3076664f
bapuoldtown <58926512+bapuoldtown@users.noreply.github.com>
2025-06-24 17:02:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
+@app.get("/Srikhestra-Dham")
 def hello():
     return {"msg": "Jai Jagannath 🚩"}
+
+@app.get("/Ekamrakshetra-Dham")
+def hi():
+    return {"msg": "Jai Shri Lingaraj 🚩"}
